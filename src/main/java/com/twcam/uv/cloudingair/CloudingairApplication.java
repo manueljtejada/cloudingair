0ec7a72ecb953460903f9455cc9d4c5d5d770069
Manuel Tejada <manute32@gmail.com>
2019-05-26 20:40:31 +0200

    Initial commit

---
+++ src/main/java/com/twcam/uv/cloudingair/CloudingairApplication.java
@@ -0,0 +1,13 @@
+package com.twcam.uv.cloudingair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudingairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudingairApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twcam/uv/cloudingair/CloudingairApplicationTests.java
@@ -0,0 +1,16 @@
+package com.twcam.uv.cloudingair;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CloudingairApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
