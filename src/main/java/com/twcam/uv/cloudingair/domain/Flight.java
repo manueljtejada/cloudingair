fdf15452507cf2e34bf172b649b2e99df048b5da
Manuel Tejada <manute32@gmail.com>
2019-05-28 18:45:54 +0200

    Implement Flight entity

--- src/main/java/com/twcam/uv/cloudingair/domain/Flight.java
+++ src/main/java/com/twcam/uv/cloudingair/domain/Flight.java
@@ -1,3 +1,65 @@
 package com.twcam.uv.cloudingair.domain;
 
-public class Flight {}
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "flights")
+public class Flight {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+
+  @Column(name = "number_flight")
+  @Size(max = 10)
+	private String numberFlight;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "origin")
+	private Airport origin;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "destination")
+	private Airport destination;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "plane")
+	private Plane plane;
+
+  @Column(name = "reservation_start_date")
+	private LocalDate reservationStartDate;
+
+  @Column(name = "departure_date")
+	private LocalDate departureDate;
+
+  @Column(name = "boarding_time")
+	private LocalTime boardingTime;
+
+  @Column(name = "departure_time")
+	private LocalTime departureTime;
+
+  @Column(name = "duration")
+	private int duration;
+
+  @Column(name = "company")
+  @Size(max = 30)
+	private String company;
+}
