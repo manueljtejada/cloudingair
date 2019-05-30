8a1d863465dec77b2b91416eacd70cfa8fad2d2f
Manuel Tejada <manute32@gmail.com>
2019-05-30 20:32:14 +0200

    WIP: Q1

---
+++ src/main/java/com/twcam/uv/cloudingair/repository/FlightRepository.java
@@ -0,0 +1,22 @@
+package com.twcam.uv.cloudingair.repository;
+
+import java.util.List;
+
+import com.twcam.uv.cloudingair.domain.Airport;
+import com.twcam.uv.cloudingair.domain.Flight;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FlightRepository extends JpaRepository<Flight, Integer> {
+
+  @Query("select f from Flight f where f.origin = :origin and f.destination = :destination")
+  public List<Flight> findFlights(
+    @Param("origin") Airport origin,
+    @Param("destination") Airport destination
+  );
+
+}
