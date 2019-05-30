8a403d8d0e41b8b96d941a9815a8852125aec1fe
aldorea <adomenechreal@gmail.com>
2019-05-30 16:32:23 +0200

    add agency emtity

---
+++ src/main/java/com/twcam/uv/cloudingair/domain/Agency.java
@@ -0,0 +1,23 @@
+package com.twcam.uv.cloudingair.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Agency {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String userName;
+	
+}
