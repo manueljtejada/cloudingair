a9ce040fb694059fd0379e1507d539651b5b2da4
Manuel Tejada <manute32@gmail.com>
2019-05-31 20:20:51 +0200

    Fix reservation relations

--- src/main/java/com/twcam/uv/cloudingair/domain/ReservationPassenger.java
+++ src/main/java/com/twcam/uv/cloudingair/domain/ReservationPassenger.java
@@ -1,5 +1,6 @@
 package com.twcam.uv.cloudingair.domain;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -10,12 +11,20 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "reservation_passengers")
 public class ReservationPassenger {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
+
   @Column(name = "checked_in")
   private boolean checkedIn;
 
@@ -24,8 +33,15 @@ public class ReservationPassenger {
 
   @Column(name = "bag_number")
   private int bagNumber;
-  
+
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "seat")
   private Seat seat;
+
+  @ManyToOne
+  @JoinColumn(name = "reservation")
+  private Reservation reservation;
+
+  @ManyToOne(cascade = { CascadeType.MERGE, CascadeType.PERSIST })
+  private Passenger passenger;
 }
