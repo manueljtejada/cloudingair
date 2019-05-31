8c672eb1ce49b7aaa0569abf28f543ee31ffd5fa
Manuel Tejada <manute32@gmail.com>
2019-05-31 20:20:59 +0200

    Implement Pay query

---
+++ src/main/java/com/twcam/uv/cloudingair/repository/ReservationRepository.java
@@ -0,0 +1,19 @@
+package com.twcam.uv.cloudingair.repository;
+
+import javax.transaction.Transactional;
+
+import com.twcam.uv.cloudingair.domain.Reservation;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Transactional
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
+  @Modifying
+  @Query("UPDATE Reservation r SET r.paid = True WHERE r.id = :reservationId")
+  public void pay(@Param("reservationId") int reservationId);
+}
