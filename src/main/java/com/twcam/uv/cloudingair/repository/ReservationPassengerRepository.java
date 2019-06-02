ddaaf0e3afd1df6468ad317e7ba0aeec398aade4
Manuel Tejada <manute32@gmail.com>
2019-06-02 19:29:35 +0200

    Test Q6

--- src/main/java/com/twcam/uv/cloudingair/repository/ReservationPassengerRepository.java
+++ src/main/java/com/twcam/uv/cloudingair/repository/ReservationPassengerRepository.java
@@ -4,16 +4,11 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
-import com.twcam.uv.cloudingair.domain.Agency;
-import com.twcam.uv.cloudingair.domain.Flight;
 import com.twcam.uv.cloudingair.domain.Passenger;
-import com.twcam.uv.cloudingair.domain.Reservation;
 import com.twcam.uv.cloudingair.domain.ReservationPassenger;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Transactional
