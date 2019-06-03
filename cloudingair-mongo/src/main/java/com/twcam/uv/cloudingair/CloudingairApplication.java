9b06fa630046f81529ae8fe872d153232aff1885
Manuel Tejada <manute32@gmail.com>
2019-06-03 17:45:49 +0200

    Add SecurityCheckRepository

--- cloudingair-mongo/src/main/java/com/twcam/uv/cloudingair/CloudingairApplication.java
+++ cloudingair-mongo/src/main/java/com/twcam/uv/cloudingair/CloudingairApplication.java
@@ -1,13 +1,34 @@
 package com.twcam.uv.cloudingair;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import com.twcam.uv.cloudingair.domain.Boarding;
+import com.twcam.uv.cloudingair.domain.SecurityCheck;
+import com.twcam.uv.cloudingair.domain.Ticket;
+import com.twcam.uv.cloudingair.repository.SecurityCheckRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CloudingairApplication {
+public class CloudingairApplication implements CommandLineRunner {
+
+	@Autowired
+	SecurityCheckRepository securityRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudingairApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Boarding boarding = new Boarding(1, LocalDateTime.now(), "50A", 5);
+		Ticket ticket = new Ticket(1, null, null, null);
+		securityRepository.save(new SecurityCheck(1, LocalDate.of(2019, 06, 03), LocalTime.now(), ticket));
 	}
 
 }
