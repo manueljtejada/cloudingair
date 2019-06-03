0f61740daae47af1ba950254693ce95cccaeb3a0
aldorea <adomenechreal@gmail.com>
2019-06-03 18:34:35 +0200

    adding boarding controller

---
+++ cloudingair-mongo/src/main/java/com/twcam/uv/cloudingair/repository/BoardingRepository.java
@@ -0,0 +1,14 @@
+package com.twcam.uv.cloudingair.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.twcam.uv.cloudingair.domain.Boarding;
+
+
+public interface BoardingRepository extends MongoRepository<Boarding, Integer>{
+	
+	public Boarding findById(int id);
+
+}
