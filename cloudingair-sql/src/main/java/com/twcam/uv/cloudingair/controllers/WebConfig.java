c24eac5007e039ba708ac537bd8c21868efd9766
Manuel Tejada <manute32@gmail.com>
2019-06-04 19:28:26 +0200

    Disable csrf

--- cloudingair-sql/src/main/java/com/twcam/uv/cloudingair/controllers/WebConfig.java
+++ cloudingair-sql/src/main/java/com/twcam/uv/cloudingair/controllers/WebConfig.java
@@ -1,14 +1,21 @@
 package com.twcam.uv.cloudingair.controllers;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*").allowCredentials(false)
 				.maxAge(3600);
 	}
+
+	@Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable();
+  }
 }
