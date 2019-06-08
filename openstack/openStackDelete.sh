0adad8793873b3d939b54edbbf1de212e547f795
aldorea <adomenechreal@gmail.com>
2019-06-09 00:01:36 +0200

    addign bash

---
+++ openstack/openStackDelete.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#Definición de variables
+network="cloudingair-6"
+subnetwork="cloudingair-6-subnetwork"
+router="clouding-air-6-routing"
+securityGroup="lab1"
+flavor="lab1"
+machine="cloudingair-6-machine"
+ip=$(openstack floating ip list -c "Floating IP Address" -f value)
+port=$(openstack port list --router $router -f value | awk '{print $1;}')
+dockerNetwork="cloauding-netowrk"
+
+
+#Borramos todas las Docker machine localmente
+sudo docker-machine rm $(docker-machine ls) --force
+
+#Borramos la red creado para docker swarm
+sudo docker network rm $dockerNetwork
+
+# Borramos la ip flotante
+openstack floating ip delete $ip
+
+# Borramos  el puerto asiganado al router
+openstack router remove port $router $port
+
+# Borramos el router
+openstack router delete $router
+
+# Borramos las instancias
+openstack server delete $machine
+
+# Borrar las instancias
+# for i in `seq 1 2`
+#         do
+#           openstack server delete tomcat-$i
+#         done
+
+# Borrar el grupo de seguridad
+openstack security group delete $securityGroup
+
+# Borrar la subred
+openstack subnet delete $subnetwork
+
+# Borrar la red
+openstack network delete $network
+
--- openstack/openstack.sh
+++ openstack/openstack.sh
@@ -8,72 +8,66 @@ subnetwork="cloudingair-6-subnetwork"
 router="clouding-air-6-routing"
 securityGroup="lab1"
 flavor="lab1"
+machine="cloudingair-6-machine"
 
 
-#Creamos la red
-echo "Creating network $network"
+# Creamos la red
+echo "Creating neFtwork $network"
 openstack network create $network
 
-#Creamos la subred
-echo "Creating subnetwork $subnetwork"
-openstack subnet create --subnet-range 10.2.0.0/24 --network $network --dns-nameserver 8.8.8.8 $subnetwork
+# #Creamos la subred
+# echo "Creating subnetwork $subnetwork"
+# openstack subnet create --subnet-range 10.2.0.0/24 --network $network --dns-nameserver 8.8.8.8 $subnetwork
 
-#Creamos el router
-echo "Creating router $router"
-openstack router create $router
+# Creamos el router
+ echo "Creating router $router"
+ openstack router create $router
 
-#Añadimos la subnet al router
-echo "Adding subnetwork $subnetwork to the router  $router"
-openstack router add subnet $router $subnetwork
+# #Añadimos la subnet al router
+ echo "Adding subnetwork $subnetwork to the router $router"
+ openstack router add subnet $router $subnetwork
 
-#Añadimos el gateway al router
-echo "Adding the gateway to the router $router"
-neutron router-gateway-set $router external-network
+# #Añadimos el gateway al router
+ echo "Adding the gateway to the router $router"
+ neutron router-gateway-set $router external-network
 
-#Creamos grupos de seguridad
-echo "Creating security group $securityGroup"
-openstack security group create $securityGroup
+# #Creamos grupos de seguridad
+ echo "Creating security group $securityGroup"
+ openstack security group create $securityGroup
 
-#Creamos la reglas de acceso
-echo "Creating security rules"
-openstack security group rule create --proto icmp $securityGroup
-openstack security group rule create --proto tcp --dst-port 22 $securityGroup
-openstack security group rule create --proto tcp --dst-port 80:80  $securityGroup --remote-ip 0.0.0.0/0
-openstack security group rule create --proto tcp --dst-port 3306  $securityGroup
-openstack security group rule create --proto tcp --dst-port 27017  $securityGroup
-openstack security group rule create --proto tcp --dst-port 5001  $securityGroup
+# #Creamos la reglas de acceso
+ echo "Creating security rules"
+ openstack security group rule create --proto icmp $securityGroup
+ openstack security group rule create --proto tcp --dst-port 22 $securityGroup
+ openstack security group rule create --proto tcp --dst-port 80:80  $securityGroup --remote-ip 0.0.0.0/0
+ openstack security group rule create --proto tcp --dst-port 3306  $securityGroup
+ openstack security group rule create --proto tcp --dst-port 27017  $securityGroup
+ openstack security group rule create --proto tcp --dst-port 5001  $securityGroup
 
 #Creamos el una vm que contega el demonio de Docker
-docker-machine create \
-    --driver openstack \
-    --openstack-flavor-name $flavor \
-    --openstack-image-name ubuntu-4-docker \
-    --openstack-net-name cloudingair-6 \
-    --openstack-floatingip-pool external-network \
-    --openstack-ssh-user ubuntu \
-    --openstack-sec-groups default,lab1 \
-    --openstack-domain-name Default \
-    cloudingair-6-machine
+#Hay que buscar la manera de compartir las claves ssh
 
-#Accedemos a la vm creada
-#docker-machine ssh cloudingair-6-machine
+ docker-machine create --driver openstack --openstack-flavor-name lab1 --openstack-image-name ubuntu-4-docker --openstack-net-name cloudingair-6 --openstack-floatingip-pool external-network --openstack-ssh-user ubuntu --openstack-sec-groups lab1,default --openstack-domain-name Default $machine
 
-#Mapeamos a nuestro terminal
+# Accedemos a la vm creada
+ docker-machine ssh cloudingair-6-machine
+
+# Mapeamos a nuestro terminal
 echo "Creating Docker host "
-eval $(docker-machine env cloudingair-6-machine)
+eval $(sudo docker-machine env $machine)
 
 #Inciamos el modo docker swarm
 docker swarm init
 
 #Creamos la red overlay
-docker network create -d overlay --attachable cloudingair-6-network
+docker network create -d overlay --attachable $network
 
 #Lanzamos los servicios en el clúster creado
 docker stack deploy -c docker-compose.yml cloudingair
 
-#Compartir la vm creada con docker-machine con tus compañeros
+# Compartir la vm creada con docker-machine con tus compañeros
 # docker-machine create \
-#   --driver generic \
-#   --generic-ip-address=YOUR_SERVER_IP \
-#   --generic-ssh-key PATH_TO_YOUR_SSH_KEY \
-#   my_project
+# --driver generic \
+# --generic-ip-address=YOUR_SERVER_IP \
+# --generic-ssh-key PATH_TO_YOUR_SSH_KEY \
+# my_project
