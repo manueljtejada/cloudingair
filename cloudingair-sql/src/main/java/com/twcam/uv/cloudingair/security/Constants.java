a210c2f9a0e33a700e48d76dcddddf0349b8f4ea
Manuel Tejada <manuel.tejada@rithmi.com>
2019-06-09 12:37:02 +0200

    Change jwt issuer

--- cloudingair-sql/src/main/java/com/twcam/uv/cloudingair/security/Constants.java
+++ cloudingair-sql/src/main/java/com/twcam/uv/cloudingair/security/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
 	public static final String TOKEN_BEARER_PREFIX = "Bearer ";
 
 	// JWT
-	public static final String ISSUER_INFO = "http://localhost:8080/";
+	public static final String ISSUER_INFO = "http://cloudingair-sql:8080";
 	public static final String SUPER_SECRET_KEY = "1234";
 	public static final long TOKEN_EXPIRATION_TIME = 864_000_000; // 10 day
 
